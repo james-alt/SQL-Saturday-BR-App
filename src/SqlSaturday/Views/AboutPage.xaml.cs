1b297ba0159035c424f103c88ae1f7efb43bd481
James Alt <james.alt@alterityllc.com>
2018-04-11 22:29:38 -0500

    Initial Commit

---
+++ src/SqlSaturday/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SqlSaturday
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
