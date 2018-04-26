9ffebf6d2440b5a7d28f7398d8b0a80141796025
James Alt <james.alt@me.com>
2018-04-26 09:06:18 -0500

    Added Saved Session and Tweet Basics

---
+++ src/SqlSaturday.Core/Entities/Tweet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SqlSaturday.Core.Shared;
+
+namespace SqlSaturday.Core.Entities
+{
+    public class Tweet
+        : BaseEntity<string>
+    {
+        public string Message { get; set; }
+        public string Username { get; set; }
+    }
+}
