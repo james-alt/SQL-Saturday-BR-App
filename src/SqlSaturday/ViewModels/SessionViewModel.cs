0f264a26c8cb6ac1a079d3268e3f231a50963dd4
James Alt <james.alt@alterityllc.com>
2018-07-16 18:31:21 -0500

    Session and Speaker Pages

---
+++ src/SqlSaturday/ViewModels/SessionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SqlSaturday.Core.Entities;
+using MvvmHelpers;
+
+namespace SqlSaturday.ViewModels
+{
+    public class SessionViewModel
+        : BaseViewModel
+    {
+        public SessionViewModel(
+            Session session)
+        {            
+            Session = session;
+
+            Title = "Session";
+        }
+
+        public Session Session { get; }
+    }
+}
--- src/SqlSaturday/Views/SpeakerPage.xaml.cs
+++ src/SqlSaturday/Views/SpeakerPage.xaml.cs
@@ -8,18 +8,6 @@ namespace SqlSaturday.Views
     {
         private SpeakerViewModel viewModel;
 
-        public SpeakerPage()
-        {
-            var speaker = new Speaker
-            {
-                Name = "James Alt",
-                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed mattis mauris, at luctus lectus. Nunc nec tortor magna. In egestas ipsum in diam ultrices sagittis. Cras euismod volutpat risus in pretium. Nunc sodales nulla nec enim feugiat, sed scelerisque elit cursus. Aliquam facilisis vitae massa ac venenatis. Vestibulum rhoncus tortor ac viverra vehicula. Mauris sed suscipit nisi. Quisque blandit sapien vitae ante luctus rhoncus. Ut ut turpis mi.",
-                Id = "0"
-            };
-
-            BindingContext = viewModel = new SpeakerViewModel(speaker);
-        }
-
         public SpeakerPage(
             Speaker speaker)
         {
